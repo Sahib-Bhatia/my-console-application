6f17af8037d1a722fe07fc797dd3012fefefe254
Sahib Bir Singh Bhatia <sahib.bhatia@credera.co.uk>
2023-09-21 14:14:49 +0100

    Add Serilog Logging

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System.Text.Json;
 using ConsoleApp;
 using RestSharp;
+using Serilog;
 
-var client = new RestClient(); 
+var client = new RestClient();
+
+Log.Logger = new LoggerConfiguration().WriteTo.Console().MinimumLevel.Debug().CreateLogger();
 
 var repositories = ProcessRepositoriesAsync(client);
 
@@ -17,10 +20,17 @@ foreach (var repository in repositories)
     Console.WriteLine();
 }
 
-static List<Repository> ProcessRepositoriesAsync (RestClient client)
+static List<Repository> ProcessRepositoriesAsync(RestClient client)
 {
+    Log.Information("Sending a get request to the github api");
     var request = new RestRequest("https://api.github.com/orgs/dotnet/repos");
     var response = client.ExecuteGet(request);
+    if (!response.IsSuccessful)
+    {
+        Log.Error($"Something went wrong. Error Message {response.StatusCode}");
+        return null;
+    }
+    Log.Information("Data Fetching Successful");
     var data = JsonSerializer.Deserialize<List<Repository>>(response.Content!);
 
     return data ?? new List<Repository>();
